079e905fbe0dfee009efddeac0431be8f80ac3bc
Khaleb <khope7@hotmail.com>
2025-09-18 16:56:47 +0200

    Adjusted welcome message

--- e-commerce-api-project/src/components/HomePage.jsx
+++ e-commerce-api-project/src/components/HomePage.jsx
@@ -5,7 +5,7 @@ function HomePage() {
     return (
         <div>
             <h1>Welcome to Our E-Commerce App</h1>
-            <p>This is the place to find all your needs at one click.</p>
+            <h6>This is the place to find all your needs at one click.</h6>
         </div>
     )
 }
