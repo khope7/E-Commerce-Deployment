93e68ccd4ec72da38596b9adcd2095d59d7d8fbf
Khaleb <khope7@hotmail.com>
2025-10-01 19:25:21 +0200

    Adjusted 404 page

--- e-commerce-api-project/src/components/NotFound.jsx
+++ e-commerce-api-project/src/components/NotFound.jsx
@@ -9,16 +9,24 @@ function NotFound() {
 
         <div className="container d-grid col col-5 g-4 py-5 border rounded mt-3 justify-content-center align-items-center bg-secondary text-warning">
             <h2>404 - Not Found</h2>
-            <img src="../../assets/404NotFound.jpg" alt="404 Not Found" width={300} height={350}/>
-            <p>Oops! Looks like your lost traveler! Remember to hit the "show" button from your customer or orders list for this page to populate correctly.</p>
-            <p>
+            <img src="assets/404NotFound.jpg" alt="404 Not Found" width={300} height={350}/>
+            <h4>
+                Oops! Looks like your lost traveler!
+            </h4>
+
+            <h5>
+                Remember to hit the "show" button from your customer or order's list for this page to populate correctly.
+            </h5>
+
+            <h4>
                 You can always go back to the homepage.
-            </p>
-                <Nav>
-                    <Nav.Item>
-                        <button className='shadow rounded bg-secondary'><Nav.Link className='notFound text-white' href='/'>Homepage</Nav.Link></button>
-                    </Nav.Item>
-                </Nav>
+            </h4>
+
+            <Nav>
+                <Nav.Item>
+                    <button className='shadow rounded bg-secondary'><Nav.Link className='notFound text-white' href='/'>Homepage</Nav.Link></button>
+                </Nav.Item>
+            </Nav>
         </div>
         
     )
